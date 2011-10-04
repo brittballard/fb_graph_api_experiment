0bd7250e7438bee9a488cc76f01a5b8aaea9189f
Britton Ballard <davidbritton.ballard@gmail.com>
2011-10-04 17:13:12 -0500

    Removed an unneccessary instance variable from the spec steps.

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -3,7 +3,6 @@ Given /^I am logged in$/ do
   @oauth = mock('oauth')
   @graph = mock('graph')
   setup_mock_graph
-  @user = User.new(@graph, @uid)
   Koala::Facebook::OAuth.should_receive(:new).any_number_of_times.and_return(@oauth)
   user_info = {'access_token' => '1234567890', 'uid' => @uid}
   @oauth.should_receive(:get_user_info_from_cookie).any_number_of_times.and_return(user_info)
