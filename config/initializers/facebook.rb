23813491aed997b616402e32e78fe47ea90708ce
Britton Ballard <davidbritton.ballard@gmail.com>
2011-10-04 17:49:20 -0500

    Updating facebook initializer for heroku.

--- config/initializers/facebook.rb
+++ config/initializers/facebook.rb
@@ -1,3 +1,8 @@
-facebook_settings = YAML.load_file(Rails.root.join('config', 'facebook.yml'))[Rails.env]
-FACEBOOK_APP_ID = facebook_settings['app_id']
-FACEBOOK_SECRET_KEY = facebook_settings['secret_key']
+if Rails.env != 'production'
+  facebook_settings = YAML.load_file(Rails.root.join('config', 'facebook.yml'))[Rails.env]
+  FACEBOOK_APP_ID = facebook_settings['app_id']
+  FACEBOOK_SECRET_KEY = facebook_settings['secret_key']
+else
+  FACEBOOK_APP_ID = ENV['app_id']
+  FACEBOOK_SECRET_KEY = ENV['secret_key']
+end
